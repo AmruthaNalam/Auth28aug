3ac2497b5dd817928b7a8c3ad2a4b3e052a7b0b1
Amrutha <amruthanalam2002@gmail.com>
2025-08-28 13:55:21 +0530

    one employee has added

---
+++ src/main/java/com/auth/_workfoxtech/entitiy/Employee.java
@@ -0,0 +1,14 @@
+package com.auth._workfoxtech.entitiy;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+}
