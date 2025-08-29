90db4bd3f6f379cd2cb5d008b91040f6c1ec85de
Anand Reddy r <anandkumarreddy8456@gmail.com>
2025-08-29 11:16:34 +0530

    Refresh token has fixed

--- src/main/java/com/auth/_workfoxtech/dto/RefreshTokenRequest.java
+++ src/main/java/com/auth/_workfoxtech/dto/RefreshTokenRequest.java
@@ -1,8 +1,12 @@
 package com.auth._workfoxtech.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RefreshTokenRequest {
     private String token;
 }
