00a768bbc90edc420a3e05984775202665c73996
Anand Reddy r <anandkumarreddy8456@gmail.com>
2025-08-29 09:30:09 +0530

    Exceptions has added and handled

---
+++ src/main/java/com/auth/_workfoxtech/common/CustomExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.auth._workfoxtech.common;
+
+import com.auth._workfoxtech.util.ResponseMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Collections;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ResponseMessage> handleBadCredentials(BadCredentialsException ex) {
+        return new ResponseEntity<>(new ResponseMessage(ConstantMessage.AE_UNAUTHORIZED,Collections.emptyList(),ConstantMessage.BAD_CREDENTIALS), HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Something went wrong. Please try again.");
+    }
+}
